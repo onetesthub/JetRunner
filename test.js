2b6ef1b305d6e4bdb30925593497f6dd0c14297f
ArtTwis <art.twis@gmail.com>
2021-09-07 01:15:35 +0530

    temp file : changes in test file

--- test.js
+++ test.js
@@ -3,24 +3,25 @@ const { responseStatus } = require('./const');
 let SuiteData = require('./index.js').SuiteData;
 
 const testModule = async () => {
-  // const suiteDataRes = await (new SuiteData({ projectPath: "C:\\Users\\DELL\\Desktop\\request_test" }));
-  const suiteDataRes = await new SuiteData({
-    projectPath: '/Users/twinklegoyal/Desktop/jetman projects/New_DataStructure'
-  });
-  if (suiteDataRes.status !== responseStatus.success) {
-    console.log('error ....');
-    console.error(suiteDataRes);
-    return suiteDataRes;
-  }
-  console.log(suiteDataRes.message);
-  const suiteData = suiteDataRes.data;
-  await suiteData.initSuites();
-  // // await suiteData.setSortedSuiteIds();
-  let sortedSuite = await suiteData.getNestedSortedSuiteIds(['cXX0PRhxHe5jVRlz']);
-  console.log('testModule -> sortedSuite :', sortedSuite);
-  // wBm5sptFeN1umK9Y
-  // // let sortedReq = await suiteData.getNestedSortedRequests(['i0i1QsukBDrEOoMa']);
-  // let sortedReq = await suiteData.getSortedRequests(['i0i1QsukBDrEOoMa']);
-  // console.log('sortedReq :>> ', sortedReq);
+	// const suiteDataRes = await (new SuiteData({ projectPath: "C:\\Users\\DELL\\Desktop\\request_test" }));
+	const suiteDataRes = await new SuiteData({
+		projectPath: '/Users/twinklegoyal/Desktop/jetman projects/New_DataStructure',
+	});
+	if (suiteDataRes.status !== responseStatus.success) {
+		console.log('error ....');
+		console.error(suiteDataRes);
+		return suiteDataRes;
+	}
+	console.log(suiteDataRes.message);
+	const suiteData = suiteDataRes.data;
+	await suiteData.initSuites();
+	// // await suiteData.setSortedSuiteIds();
+	// let sortedSuite = await suiteData.getAllSuiteIds();
+	// let sortedSuite = await suiteData.getNestedSortedSuiteIds(['GxaVJDP1l3nmJlmv']);
+	// console.log('testModule -> sortedSuite :', sortedSuite);
+	// wBm5sptFeN1umK9Y
+	let sortedReq = await suiteData.getNestedSortedRequests(['GxaVJDP1l3nmJlmv']);
+	// let sortedReq = await suiteData.getSortedRequests(['GxaVJDP1l3nmJlmv']);
+	console.log('sortedReq :>> ', sortedReq);
 };
 testModule();
